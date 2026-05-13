b2aa6f881d5682255fa70fc77bd7a950efd95e7f
brenda <brenda@synetechworld.com>
2026-05-13 17:23:23 +0200

    casi el final del tutorial 3

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -18,6 +18,9 @@ class Question(models.Model):
     # cómo se mostrará este objeto en Django admin, shell, etc
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -34,7 +37,6 @@ class Choice(models.Model):
     def __str__(self):
             return self.choice_text
     
-    def was_published_recently(self):
-            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 
 
--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -6,15 +6,18 @@ from . import views
  Eso significa:
  /polls/        -> entra aquí
  /polls/otra/   -> también podría entrar aquí si existiera esa ruta """
+
+
+""" me dice que las rutas de este archivo pertenecen a la app polls, asi evitamos connflictos por si otra app tmb tiene una ruta llamada detail 
+por ejemplo: polls:detail, blog:detail, products:detail.     """
+app_name = "polls"
+
 urlpatterns = [
     # Ruta vacía "" porque el prefijo /polls/ ya lo puso mysite/urls.py.
     # URL final: http://127.0.0.1:8000/polls/
     # Cuando alguien entra ahí, Django busca la funcion index dentro del archivo views y ejecuta: views.index(request) 
     path("", views.index, name="index"),
-    # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
