892d97fa05fe77617704571376ba180d772514f8
brenda <brenda@synetechworld.com>
2026-05-14 08:48:02 +0200

    otro commit

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # URL final: http://127.0.0.1:8000/polls/
     # Cuando alguien entra ahí, Django busca la funcion index dentro del archivo views y ejecuta: views.index(request) 
 path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"), #cambiamos question_id por pk
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
