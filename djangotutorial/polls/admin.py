f19e301433d2bc4c3267e8db31e0136c94006f2a
brenda <brenda@synetechworld.com>
2026-05-14 11:40:06 +0200

    Personalizar el orden de los campos mostrados en el panel admin

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Question
-#con esto registramos el modelo Question en admin para que django sepa que debe mostrarlo en el index de admin
-admin.site.register(Question)#con esto podremos crear preguntas, editarlas y borrarlas
+
+'''Controla el orden de los campos dentro del formulario del admin
+Antes Django mostraba los campos en el orden del modelo:    - question_text
+                                                            - pub_date 
+Y ahora forzamos a que aparzca primero pub_date y luego question_text                                                            '''
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+#registramos el modelo Question en admin pero usando la configuracion personalizada QuestionAdmin
+admin.site.register(Question, QuestionAdmin)#con esto podremos crear preguntas, editarlas y borrarlas
