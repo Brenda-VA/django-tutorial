aa2ab3af893ce6355b22ea85d8b921834970bfec
brenda <brenda@synetechworld.com>
2026-05-14 11:45:43 +0200

    usar fieldsets para formularios del panel admin

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -6,7 +6,13 @@ Antes Django mostraba los campos en el orden del modelo:    - question_text
                                                             - pub_date 
 Y ahora forzamos a que aparzca primero pub_date y luego question_text                                                            '''
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    '''fieldsets permite dividir el formulario del admin en bloques
+    cada bloque es una tupla: ("Título del bloque", {"fields": [...]}) 
+    Si el titulo es none, django no muestra encabezado para ese bloque'''
+    fieldsets = [
+        (None, {"fields": ["question_text"]}), # no tiene encabezado, de frente va el contenido
+        ("Date information", {"fields": ["pub_date"]}), #muestra encabezado que dice Date information
+    ]
 
 #registramos el modelo Question en admin pero usando la configuracion personalizada QuestionAdmin
 admin.site.register(Question, QuestionAdmin)#con esto podremos crear preguntas, editarlas y borrarlas
