95b9c936d873db8f23136f48e62879bb25410b23
brenda <brenda@synetechworld.com>
2026-05-14 09:41:29 +0200

    corregir el bug encontrado por el test

--- djangotutorial/polls/models.py
+++ djangotutorial/polls/models.py
@@ -19,8 +19,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+# despues de arreglar el error probamos con python manage.py test polls para pobar de nuevo el test manualmente    
     def was_published_recently(self):
-            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
