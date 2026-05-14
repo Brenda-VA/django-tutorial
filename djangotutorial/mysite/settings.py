c230f813a1c38fd00129caa5b835e89df679b9b9
brenda <brenda@synetechworld.com>
2026-05-14 12:39:33 +0200

    configurar templates globales en el proyecto

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], #le dice a Django busca templates en djangotutorial/templates/
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
